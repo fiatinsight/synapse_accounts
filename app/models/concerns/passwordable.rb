04e02e8b542a6c8b4ee36b7dde89906e37542778
Andrew Haines <andrew@fiatinsight.com>
2020-03-06 05:17:41 -0500

    Add Passwordable concern

---
+++ app/models/concerns/passwordable.rb
@@ -0,0 +1,14 @@
+module Passwordable
+  extend ActiveSupport::Concern
+
+  included do
+    after_commit :generate_password, on: :create
+  end
+
+  def generate_password
+    if !self.encrypted_password.present?
+      password = Devise.friendly_token.first(10)
+      self.update(password: password, password_confirmation: password)
+    end
+  end
+end
