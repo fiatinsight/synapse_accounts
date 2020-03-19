97212f1883262b8058bb4f6b83e8a3e4fc116bb6
Andrew Haines <andrew@fiatinsight.com>
2020-03-19 04:54:09 -0400

    Add Locateable concern

---
+++ app/models/concerns/locateable.rb
@@ -0,0 +1,25 @@
+module Locateable # NOTE: `Addressable` conflicts w/ another class
+  extend ActiveSupport::Concern
+
+  # Requires validation for :address_line_1, :city, :state, and :zip on model
+
+  def full_address_string
+    if self.address_line_2?
+      string = "#{self.address_line_1}, #{self.address_line_2}, #{self.city}, #{self.state} #{self.zip}"
+    else
+      string = "#{self.address_line_1}, #{self.city}, #{self.state} #{self.zip}"
+    end
+
+    string
+  end
+
+  def full_address_html
+    if self.address_line_2?
+      string = "#{self.address_line_1}<br>#{self.address_line_2}<br>#{self.city}, #{self.state} #{self.zip}"
+    else
+      string = "#{self.address_line_1}<br>#{self.city}, #{self.state} #{self.zip}"
+    end
+
+    string
+  end
+end
